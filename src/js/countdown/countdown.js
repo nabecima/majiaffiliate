20680620bf87884cb8eb4a4bbc180fecc7c130c7
nabecima <nabecima8511gk@gmail.com>
2023-06-18 03:01:51 +0900

    tmp

--- src/js/countdown/countdown.js
+++ src/js/countdown/countdown.js
@@ -15,6 +15,21 @@ const countDownId = setInterval(() => {
 
   if (gap <= 0) {
     clearInterval(countDownId);
+    const insertHtml = `<div class="p-finished">
+      <p>
+        マジアフィの
+        <br />
+        募集は終了しました
+      </p>
+      <div>
+        <p>ご相談・ご質問はLINEにてご連絡お願いします。</p>
+        <span>↓↓↓</span>
+        <p>
+          <a href="#">お問い合わせはこちら</a>
+        </p>
+      </div>
+    </div>`;
+    document.getElementById("countDown").innerHTML = insertHtml;
   }
 
   const second = 1000;
